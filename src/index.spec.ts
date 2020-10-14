d284680eed407bf40516be89d5561ccb56a1d07d
Adrian Mroz <adrianmroz@gmail.com>
2020-10-14 11:24:24 +0200

    three flavours of operators, first - accumulator

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,12 +1,35 @@
 import { Stream } from "xstream";
 import { get } from "svelte/store";
-import { accumB } from "./index";
+import { accumRC } from "./index";
 
-describe("accumB", () => {
-  it("works", () => {
+function delay<T>(ms: number, value: T): Promise<T> {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(value);
+    }, ms);
+  });
+}
+
+function noop(a: any): void { }
+
+describe("accumRC", () => {
+  it("sync case", () => {
     const stream = Stream.of(1);
-    const behaviour = accumB(stream, 0);
+    const behaviour = accumRC(stream, 0);
     const value = get(behaviour);
     expect(value).toEqual(1);
+  });
+
+  it("async case", async () => {
+    const stream = Stream.fromPromise(delay(10, 1));
+    const behaviour = accumRC(stream, 0);
+    // need sub to keep stream running
+    const unsub = behaviour.subscribe(noop);
+    const value = get(behaviour);
+    expect(value).toEqual(0);
+    await delay(10, null);
+    const newValue = get(behaviour);
+    expect(newValue).toEqual(1);
+    unsub();
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { Stream } from "xstream";
-import { Readable, readable } from "svelte/store";
+import { Readable, readable, writable } from "svelte/store";
+import { onDestroy } from "svelte";
 
-export function accumB<T>(stream: Stream<T>, init: T): Readable<T> {
+type ManualStore<T, S extends Readable<T> = Readable<T>> = S & { destroy: () => void };
+
+export function accumRC<T>(stream: Stream<T>, init: T): Readable<T> {
     return readable(init, set => {
         const sub = stream.subscribe({
             next: value => {
@@ -10,4 +13,25 @@ export function accumB<T>(stream: Stream<T>, init: T): Readable<T> {
         });
         return () => sub.unsubscribe();
     });
+}
+
+export function accumManual<T>(stream: Stream<T>, init: T): ManualStore<T> {
+    const { set, subscribe } = writable(init);
+    const sub = stream.subscribe({
+        next: value => {
+            set(value);
+        }
+    });
+    const destroy = () => sub.unsubscribe();
+
+    return {
+        subscribe,
+        destroy
+    };
+}
+
+export function accumAuto<T>(stream: Stream<T>, init: T): Readable<T> {
+    const store = accumManual(stream, init);
+    onDestroy(() => store.destroy());
+    return store;
 }
