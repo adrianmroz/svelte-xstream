5d02b4ac436c3fb600957e932ab2a847c3864435
Adrian Mroz <adrianmroz@gmail.com>
2020-10-16 01:21:08 +0200

    auto and manual version of flatmap

--- src/flatmap.ts
+++ src/flatmap.ts
@@ -4,7 +4,7 @@ import { onDestroy } from "svelte";
 import { ManualStore } from "./manual-store";
 
 export function flatmapRC<S, T>(
-  store: Readable<T>, 
+  store: Readable<T>,
   f: (x: T) => Stream<S>,
   init: S
 ): Readable<S> {
@@ -13,15 +13,45 @@ export function flatmapRC<S, T>(
     const unsub = store.subscribe(x => {
       const stream = f(x);
       if (innerSub) innerSub.unsubscribe();
-      innerSub = stream.subscribe({
-        next: value => {
-          set(value);
-        }
-      });
+      innerSub = stream.subscribe({ next: set });
     });
     return () => {
       if (innerSub) innerSub.unsubscribe();
       unsub();
-    }
+    };
   });
+}
+
+export function flatmapManual<S, T>(
+  store: Readable<T>,
+  f: (x: T) => Stream<S>,
+  init: S
+): ManualStore<S> {
+  const { set, subscribe } = writable(init);
+  let innerSub: Subscription;
+  const unsubscribe = store.subscribe(x => {
+    const stream = f(x);
+    if (innerSub) innerSub.unsubscribe();
+    innerSub = stream.subscribe({ next: set });
+  });
+
+  const destroy = () => {
+    if (innerSub) innerSub.unsubscribe();
+    unsubscribe();
+  };
+
+  return {
+    subscribe,
+    destroy
+  };
+}
+
+export function flatmapAuto<S, T>(
+  store: Readable<T>,
+  f: (x: T) => Stream<S>,
+  init: S
+): Readable<S> {
+  const behavior = flatmapManual(store, f, init);
+  onDestroy(() => behavior.destroy());
+  return behavior;
 }
