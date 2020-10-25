dad440a2eb6b78d10ae7a0cb04da37fcac6f8f06
Adrian Mróź <adrianmroz@users.noreply.github.com>
2020-10-25 22:40:08 +0100

    Add prettier as linter (#10)
    * more files to gitignore
    
    * add lint script
    
    * format files
    
    * run lint on ci
    
    * add prettier to deps, duh

--- src/to-stream.ts
+++ src/to-stream.ts
@@ -1,15 +1,15 @@
-import { Readable } from 'svelte/store';
-import { Stream, Producer, Listener } from 'xstream';
+import { Readable } from "svelte/store";
+import { Stream, Producer, Listener } from "xstream";
 
 export function toStream<T>(store: Readable<T>): Stream<T> {
   let unsub: (() => void) | null = null;
   const producer: Producer<T> = {
     start(listener: Listener<T>) {
-      unsub = store.subscribe(value => listener.next(value));
+      unsub = store.subscribe((value) => listener.next(value));
     },
     stop() {
       if (unsub) unsub();
-    }
+    },
   };
   return Stream.create<T>(producer);
 }
