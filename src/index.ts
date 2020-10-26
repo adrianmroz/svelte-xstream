2084600c4c405d6b9a01f7ee5e1c33f6c3eaa61f
Adrian Mroz <adrianmroz@gmail.com>
2020-10-26 23:17:02 +0100

    reexport everything

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
 export { foldpAuto, foldpManual, foldpRC } from "./foldp";
 export { accumAuto, accumManual, accumRC } from "./accum";
+export { switchHoldRC, switchHoldManual, switchHoldAuto } from "./switch-hold";
 export { switchMap } from "./switch-map";
+export { switchB, switchE } from "./switch";
 export { toStream } from "./to-stream";
+export { gate } from "./gate";
+export { apply } from "./apply";
+export { filterApply } from "./filter-apply";
+export { tag } from "./tag";
+export { flatMap, flatten, map, of, zip } from "./store-utils/store-utils";
