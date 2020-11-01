11c275835f59d6023d74d4ca84bade15ca9be145
Adrian Mroz <adrianmroz@gmail.com>
2020-11-01 14:24:38 +0100

    use different typing for addEventListener and make tracker polymorphic over event name instead of type of returned event

--- src/tracker.ts
+++ src/tracker.ts
@@ -1,19 +1,20 @@
 import { Stream } from "xstream";
 
-export function tracker(
-  eventType: string,
+export function tracker<K extends keyof HTMLElementEventMap>(
+  eventType: K,
   options?: boolean | AddEventListenerOptions
 ) {
   const stream: Stream<Event> = Stream.never();
 
   function tracker(node: HTMLElement) {
-    const handler = (event: Event) => stream.shamefullySendNext(event);
+    const handler = (event: HTMLElementEventMap[K]) =>
+      stream.shamefullySendNext(event);
 
     node.addEventListener(eventType, handler, options);
 
     return {
       destroy: () => {
-        // node.removeEventListener(eventType, handler);
+        node.removeEventListener(eventType, handler);
         stream.shamefullySendComplete();
       },
     };
